063d3932912804c6b9d7dad0b3d5f151321d2760
Isaac <Isaac2gonzalez424@gmail.com>
2026-07-18 14:30:48 -0700

    'Welcome' messaging fixed

--- main.java
+++ main.java
@@ -8,22 +8,35 @@ public class main
         System.out.println("Welcome to the Adventure Game!\n (Press 'Enter' to Continue, or type 'Quit' to exit the game)");
 
         // Scanner for user input
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);        
+        String response = scanner.nextLine();
 
+     
+        while (!response.equalsIgnoreCase("quit") || !response.equalsIgnoreCase("enter"))
+        {  
+            System.out.println("Incorrect Response. To play: 'Enter' To exit: 'quit'");
+            response = scanner.nextLine();
+
+            if (response.equalsIgnoreCase("quit"))
+            {
+                System.out.println("Goodbye!");
+                System.exit(0);
+            }
+            else if (response.equalsIgnoreCase("enter"))
+            {
+                // Continue to Game
+                break;
+            }
+        }
+        
+        
         // Class Instatiation
         Game game = new Game();
         Player player = new Player();
 
         // Run Game
         while (player.getHealth() > 0)
-        {
-            //Check for "Quit" Player keyword
-            if ( scanner.nextLine().toLowerCase().equals("quit"))
-            {
-                System.out.println("Quitting Game...");
-                return;
-            }
-            
+        {   
             // GAME LOGIC
             System.out.print("Name: ");
             player.setName(scanner.nextLine());
@@ -33,8 +46,14 @@ public class main
             System.out.println("An enemy appears! Prepare for battle!");
 
             game.battle(scanner, player, new Enemy());
+            
+            //Check for "Quit" Player keyword
+            if (scanner.nextLine().toLowerCase().equals("quit"))
+            {
+                System.out.println("Quitting Game...");
+                return;
+            }
         }
-
         scanner.close();
     }
 }
